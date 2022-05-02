62ef05582505bce291fb70776057fb9a0a2fb3ae
marensmeets <maren.smeets@vlaanderen.be>
2022-05-02 15:53:18 +0200

    requiredFields

--- demoQaTest.js
+++ demoQaTest.js
@@ -3,11 +3,17 @@
 const { webdriver, Builder, By, Key, until } = require('selenium-webdriver');
 require('chromedriver');
 
-async function example () {
+async function inputForm () {
     let driver = await new Builder().forBrowser('chrome').build();
     await driver.get('https://demoqa.com/automation-practice-form');
     await driver.findElement(By.id('firstName')).sendKeys('Voornaam');
+    await driver.findElement(By.id('lastName')).sendKeys('Familienaam');
+    await driver.findElement(By.id('userEmail')).sendKeys('email@adr.es');
+    await driver.findElement(By.css('label[for="gender-radio-2"]')).click();
+    await driver.findElement(By.id('userNumber')).sendKeys('0123456789');
+                
+    
     await driver.quit();
 }
 
-example ();
+inputForm ();
